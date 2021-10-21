74ffb6e9a3c86316443c3e802c945b62e7425b6a
Brandon Hawfitch <brandonhawfitch@gmail.com>
2021-10-21 09:33:42 -0500

    Structure modifications

--- src/app/entities/preferences.ts
+++ src/app/entities/preferences.ts
@@ -10,6 +10,8 @@ export enum Category {
  * Any scale item; anything that can possess a meaningful value on a scale
  */
 export class Item {
+  public description: string = '';
+
   constructor(public name: string = '', public value: number = 0) {}
 }
 
@@ -32,19 +34,19 @@ export abstract class Scale {
 
   /**
    * Returns a difference value between two items that is calculated in relation to the scale type
-   * @param cat1
-   * @param cat2
+   * @param item1
+   * @param item2
    */
-  public abstract getDifference(cat1: Item, cat2: Item): number;
+  public abstract getDifference(item1: Item, item2: Item): number;
 
   public abstract getMaxDifferenceSum(scale: Scale): number;
 
   public getDifferenceSum(scale: Scale): number {
     let sum = 0;
-    this.items.forEach((catA: Item) => {
-      const catB: Item = scale.getItem(catA);
-      if (catB) {
-        sum += this.getDifference(catA, catB);
+    this.items.forEach((itemA: Item) => {
+      const itemB: Item = scale.getItem(itemA);
+      if (itemB) {
+        sum += this.getDifference(itemA, itemB);
       }
     });
 
@@ -55,30 +57,35 @@ export abstract class Scale {
     return 1 - this.getDifferenceSum(scale) / this.getMaxDifferenceSum(scale);
   }
 
-  public getItem(catB: Item): Item {
-    const nameB = catB.name;
-    let catX = null;
-    this.items.forEach((catA: Item) => {
-      if (catA.name === nameB) {
-        catX = catA;
+  public getItem(itemB: Item): Item {
+    const nameB = itemB.name;
+    let itemX = null;
+    this.items.forEach((itemA: Item) => {
+      if (itemA.name === nameB) {
+        itemX = itemA;
       }
     });
-    return catX;
+    return itemX;
   }
 
+  /**
+   * Returns the number of items that are shared between this scale and another
+   * @param scale The scale being compared against this scale
+   * @returns number of items that are shared
+   */
   public getNumberOfSharedItems(scale: Scale): number {
     let numItems = 0;
-    this.items.forEach((catA: Item) => {
-      const catB: Item = scale.getItem(catA);
-      if (catB) {
+    this.items.forEach((itemA: Item) => {
+      const itemB: Item = scale.getItem(itemA);
+      if (itemB) {
         numItems++;
       }
     });
     return numItems;
   }
 
-  public addItem(cat: Item): void {
-    this.items.push(cat);
+  public addItem(item: Item): void {
+    this.items.push(item);
   }
 }
 
@@ -134,13 +141,13 @@ export class Rating extends Scale {
     return this.max - this.min;
   }
 
-  constructor(category: Category, max: number = 7) {
-    super(max);
+  constructor(category: Category, max: number = 7, min: number = 1) {
+    super(max, min);
     this.category = category;
   }
 
-  public getDifference(cat1: Item, cat2: Item): number {
-    const absDif = Math.abs(cat1.value - cat2.value);
+  public getDifference(item1: Item, item2: Item): number {
+    const absDif = Math.abs(item1.value - item2.value);
     return absDif;
   }
 
@@ -148,5 +155,20 @@ export class Rating extends Scale {
     const n = this.getNumberOfSharedItems(scale);
     const maxDifSum = n * this.maxDifference;
     return maxDifSum;
+  }
+}
+
+export class Binary extends Scale {
+  constructor(category: Category) {
+    super(1, 0);
+    this.category = category;
+  }
+
+  public getDifference(item1: Item, item2: Item): number {
+    return Math.abs(item1.value - item2.value);
+  }
+
+  public getMaxDifferenceSum(scale: Scale): number {
+    return this.getNumberOfSharedItems(scale);
   }
 }
